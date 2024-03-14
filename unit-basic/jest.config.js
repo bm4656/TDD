5410633a0596e10e7f45126e068d067381549a5a
bm4656 <newjin46@gmail.com>
2024-03-15 03:42:26 +0900

    User 테스트 - Mock 에제

--- unit-basic/jest.config.js
+++ unit-basic/jest.config.js
@@ -15,7 +15,7 @@ const config = {
   // cacheDirectory: "/private/var/folders/p1/1fd7tj2x6bs96qrjqj_cysww0000gn/T/jest_dx",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: false,
+  clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   // 테스트 상세 커버리지 보기
