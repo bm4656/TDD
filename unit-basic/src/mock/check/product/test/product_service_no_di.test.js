2db66f1885f7d775d7e8bedbbeb42c490600ddc6
bm4656 <newjin46@gmail.com>
2024-03-15 00:03:44 +0900

    mock 테스트 - stub

--- unit-basic/src/mock/check/product/test/product_service_no_di.test.js
+++ unit-basic/src/mock/check/product/test/product_service_no_di.test.js
@@ -1,11 +1,11 @@
 const ProductService = require('../product_service_no_di.js');
 const ProductClient = require('../product_client.js');
-jest.mock('../product_client.js');
+jest.mock('../product_client');
 
 describe('ProductService', () => {
   const fetchItems = jest.fn(async () => [
-    { item: 'milk', available: true },
-    { item: 'water', available: false },
+    { item: '🥛', available: true },
+    { item: '🍌', available: false },
   ]);
   ProductClient.mockImplementation(() => {
     return {
@@ -20,10 +20,10 @@ describe('ProductService', () => {
     ProductClient.mockClear();
   });
 
-  it('should filter out only availabe items', async () => {
+  it('should filter out only available items', async () => {
     const items = await productService.fetchAvailableItems();
     expect(items.length).toBe(1);
-    expect(items).toEqual([{ item: 'milk', available: true }]);
+    expect(items).toEqual([{ item: '🥛', available: true }]);
   });
 
   it('test', async () => {
